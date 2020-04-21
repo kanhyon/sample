c3e9a0b499543d797d9e8c69a33e77aa1d6152f8
李康鉉 <kanhyon@kanhyonMacBook-Air.local>
2020-04-22 08:31:37 +0900

    add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_212149) do
+ActiveRecord::Schema.define(version: 2020_04_21_232845) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
